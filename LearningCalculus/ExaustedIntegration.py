2a809fd99ee058317a0048313de4e685b0d21ab3
ProjktLeao <93796926+ProjktLeao@users.noreply.github.com>
2021-12-23 16:26:47 -0800

    Update and rename CalculusIntegrationCalculation.py to ExaustedIntegration.py

--- LearningCalculus/ExaustedIntegration.py
+++ LearningCalculus/ExaustedIntegration.py
@@ -4,27 +4,27 @@
 
 #Pick a range for X
 #Pick a function
-#determine how many times the shape will be divided (itterations)
-#divide (range[1] - range[0]) by the number of itterations to get the linear rate of change of X
+#determine how many times the shape will be divided (integrations)
+#divide (range[1] - range[0]) by the number of integrations to get the linear rate of change of X
 #incrementaly add the linear rate of change of X to the lowest limit
-#do the same but decrease itterations by 1 and add the X's rate of change to the lower limit
+#do the same but decrease integrations by 1 and add the X's rate of change to the lower limit
 #get the mean(avarage) of the two values
 
 class function:
     def __init__(self, func) -> None:
         self.func = func
-    def calculate_area(self, limits, itterations):
+    def calculate_area(self, limits, y_plane=0, integrations=10**10):
         if len(limits) == 2:
-            rate_of_change = (limits[1]-limits[0])/itterations
+            rate_of_change = (limits[1]-limits[0])/integrations
             minimum_area = 0
             maximum_area = 0
             current_X = limits[0]
-            for i in range(itterations):
-                minimum_area += self.func(current_X) * rate_of_change #because Area = base*height and ROC is base
+            for i in range(integrations):
+                minimum_area += max(self.func(current_X) * rate_of_change + y_plane,0) #because Area = base*height and ROC is base
                 current_X += rate_of_change
             current_X = limits[0] + rate_of_change
-            for j in range(itterations - 1):
-                maximum_area += self.func(current_X) * rate_of_change #because Area = base*height and ROC is base
+            for j in range(integrations - 1):
+                maximum_area += max(self.func(current_X) * rate_of_change + y_plane,0) #because Area = base*height and ROC is base
                 current_X += rate_of_change
             return (minimum_area + maximum_area)/2
 
@@ -49,9 +49,9 @@ class library_info:
             "\n"
             "   -finnaly, assign the class 'function' to a variable and enter your mathematical function as a parameter.\n"
             "Then, in order to calculate, use the 'calculate_area' function inside your new variable containing the class of\n"
-            "function. Next, add your tuple/array as your first parameter and the number of itterations(int) as your second.\n"
-             "The 'itterations' parameter determines how many rectangles will be drawn inside your shape to determine area\n"
-            "(ie. the higher itteration is the higher the accuracy and the higher the cost to process. absurdly low(<10-20)\n"
+            "function. Next, add your tuple/array as your first parameter and the number of integrations(int) as your second.\n"
+             "The 'integrations' parameter determines how many rectangles will be drawn inside your shape to determine area\n"
+            "(ie. the higher integration is the higher the accuracy and the higher the cost to process. absurdly low(<10-20)\n"
             " and absurdly high(per machine basis) values are discouraged because of acuracy and performance issues respectfully\n"
             "\n"
             "\n"
@@ -59,7 +59,7 @@ class library_info:
             "believe i've optimized it great despite the fact ive been awake for roughly 17-18 hours. Enjoy! "
         )
     def _misc_info():
-        print("library name: Calculus Integration Calculator\n",
+        print("library name: ExaustedIntegration\n",
                 "Made by: Arthur Leao De Souza \n",
                 "Time made:12/3/2021(Dec) 9:06pm Sao Paulo time\n"
                 "library function: testing and familiarization of the application",
